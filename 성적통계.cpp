d6e3065fff6e34cda38d9541d0c042d00b90b440
byunw <byunw@bc.edu>
2021-11-27 12:34:22 +0900

    Update 성적통계.cpp

--- 성적통계.cpp
+++ 성적통계.cpp
@@ -22,13 +22,13 @@ int main(){
         
         //학생점수 담을 어레이
         int scores[N];
-
-        //핵생 점수 어레이에 받기 
+        
+        //학생 점수 어레이에 받기 
         for(int z=0;z<N;++z){
             cin >> scores[z];  
         }
 
-        //최고점수 찾아내고 출력해주기
+        //최고점수 찾아내기 
         int maximum=scores[0];
         for(int z=1;z<=N-1;z++){
 
@@ -38,7 +38,7 @@ int main(){
 
         }
 
-        //최저점수 찾아내고 출력해주기 
+        //최저점수 찾아내기
         int minimum=scores[0];
         for(int z=1;z<=N-1;z++){
 
@@ -51,7 +51,7 @@ int main(){
         //어레이 내림차순으로 성적 정렬 
         sort(scores,scores+N,greater<int>());
 
-        //가장 큰 인접 성적 차이 구하기 
+        //가장 큰 인접성적 차이 구하기 
         int max_adjdiff=scores[0]-scores[1];
         for(int z=0;z<=N-2;z++){
 
@@ -62,8 +62,9 @@ int main(){
             }
 
         }
-
-        //최고 점수,최저점수 차이 찾아내고 출력해주기
+        
+        
+        //최고 점수,최저점수,가장 큰 인접성적 차이 출력해주기
         cout << "Class ";
         cout << (i+1) << '\n';
         cout << "Max ";
