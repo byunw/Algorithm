9c086d1ccc878716080c5f16be2b873b270a4396
byunw <wbyun1461@gmail.com>
2023-08-10 19:12:55 +0900

    머리식히는 문제

---
+++ BOJ/VerySimple.java
@@ -0,0 +1,9 @@
+//warmup 문제 
+public class VerySimple{
+   public static void main(String[] args){
+       System.out.println("#  # #### #### #  #");
+       System.out.println("#### #  # #  # # #");
+       System.out.println("#### #  # #  # # #");
+       System.out.println("#  # #### #### #  #");
+   }
+}
